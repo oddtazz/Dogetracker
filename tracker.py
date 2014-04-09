28c5c92a49ec6140aa38a3446fddc83c4cfa32d5
Gaurav Chaturvedi <gaurav.p.chaturvedi@gmail.com>
2014-04-09 11:22:49 +0530

    No need to specify verifyval everywhere

--- tracker.py
+++ tracker.py
@@ -2,7 +2,7 @@ __author__ = 'tazz_2004'
 
 import requests
 
-def gets(url, verifyval):
+def gets(url, verifyval = True):
     try:
         r = requests.get(url, verify=verifyval)
         rjson = r.json()
@@ -11,21 +11,21 @@ def gets(url, verifyval):
         print "Something went wrong with " + url
 
 
-cryptsy = gets('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132', verifyval=True)
+cryptsy = gets('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132')
 print "Cryptsy = %.8f" % float(cryptsy['return']['markets']['DOGE']['lasttradeprice'])
 print "Sell price = %.8f \t Buy price = %.8f" % (float(cryptsy['return']['markets']['DOGE']['sellorders'][0]['price']), float(cryptsy['return']['markets']['DOGE']['buyorders'][0]['price']))
 
-bter = gets('http://data.bter.com/api/1/ticker/doge_btc', verifyval=True)
+bter = gets('http://data.bter.com/api/1/ticker/doge_btc')
 print "Bter = %.8f" % float(bter['last'])
 print "Sell price = %.8f \t Buy price = %.8f" % (float(bter['sell']), float(bter['buy']))
 
-bittrex = gets('https://bittrex.com/api/v1/public/getticker?market=BTC-DOGE', verifyval=True)
+bittrex = gets('https://bittrex.com/api/v1/public/getticker?market=BTC-DOGE')
 print "Bittrex = %.8f" % float(bittrex['result']['Last'])
 print "Sell price = %.8f \t Buy price = %.8f" % (float(bittrex['result']['Bid']), float(bittrex['result']['Ask']))
 
 prelude = gets('https://api.prelude.io/last/DOGE', verifyval=False)
 print "Prelude = %.8f" % float(prelude['last'])
 
-mintpal = gets('https://api.mintpal.com/market/stats/DOGE/BTC', verifyval=True)
+mintpal = gets('https://api.mintpal.com/market/stats/DOGE/BTC')
 print "Mintpal = %.8f" % float(mintpal[0]['last_price'])
 print "Sell price = %.8f \t Buy price = %.8f" % (float(mintpal[0]['top_bid']), float(mintpal[0]['top_ask']))
