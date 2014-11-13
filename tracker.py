f43b653495ec2040c7a45c01f04e60fe7c73f5df
Gaurav Chaturvedi <gaurav.p.chaturvedi@gmail.com>
2014-11-14 01:38:43 +0530

    Added a timer, and fixed API urls

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
-__author__ = 'tazz_2004'
+__author__ = 'oddtazz'
 
 import requests
+import threading2
 
 def gets(url, verifyval = True):
     try:
@@ -11,21 +12,36 @@ def gets(url, verifyval = True):
         print "Something went wrong with " + url
 
 
-cryptsy = gets('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132')
-print "Cryptsy = %.8f" % float(cryptsy['return']['markets']['DOGE']['lasttradeprice'])
-print "Sell price = %.8f \t Buy price = %.8f" % (float(cryptsy['return']['markets']['DOGE']['sellorders'][0]['price']), float(cryptsy['return']['markets']['DOGE']['buyorders'][0]['price']))
+def cryptsyprice():
+    cryptsy = gets('http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132')
+    print "Cryptsy = %.8f" % float(cryptsy['return']['markets']['DOGE']['lasttradeprice'])
+    print "Sell price = %.8f \t Buy price = %.8f" % (float(cryptsy['return']['markets']['DOGE']['sellorders'][0]['price']), float(cryptsy['return']['markets']['DOGE']['buyorders'][0]['price']))
 
-bter = gets('http://data.bter.com/api/1/ticker/doge_btc')
-print "Bter = %.8f" % float(bter['last'])
-print "Sell price = %.8f \t Buy price = %.8f" % (float(bter['sell']), float(bter['buy']))
+def bterprice():
+    bter = gets('http://data.bter.com/api/1/ticker/doge_btc')
+    print "Bter = %.8f" % float(bter['last'])
+    print "Sell price = %.8f \t Buy price = %.8f" % (float(bter['sell']), float(bter['buy']))
 
-bittrex = gets('https://bittrex.com/api/v1/public/getticker?market=BTC-DOGE')
-print "Bittrex = %.8f" % float(bittrex['result']['Last'])
-print "Sell price = %.8f \t Buy price = %.8f" % (float(bittrex['result']['Ask']), float(bittrex['result']['Bid']))
+def bitterexprice():
+    bittrex = gets('https://bittrex.com/api/v1.1/public/getticker?market=BTC-DOGE')
+    print "Bittrex = %.8f" % float(bittrex['result']['Last'])
+    print "Sell price = %.8f \t Buy price = %.8f" % (float(bittrex['result']['Ask']), float(bittrex['result']['Bid']))
 
-prelude = gets('https://api.prelude.io/last/DOGE', verifyval=False)
-print "Prelude = %.8f" % float(prelude['last'])
+def preludeprice():
+    prelude = gets('https://api.prelude.io/last/DOGE', verifyval=False)
+    print "Prelude = %.8f" % float(prelude['last'])
 
-mintpal = gets('https://api.mintpal.com/market/stats/DOGE/BTC')
-print "Mintpal = %.8f" % float(mintpal[0]['last_price'])
-print "Sell price = %.8f \t Buy price = %.8f" % (float(mintpal[0]['top_ask']), float(mintpal[0]['top_bid']))
+def mintpalprice():
+    mintpal = gets('https://api.mintpal.com/market/stats/DOGE/BTC')
+    print "Mintpal = %.8f" % float(mintpal[0]['last_price'])
+    print "Sell price = %.8f \t Buy price = %.8f" % (float(mintpal[0]['top_ask']), float(mintpal[0]['top_bid']))
+
+def pricetimer():
+    threading2.Timer(2.0, pricetimer).start()
+    cryptsyprice()
+    bterprice()
+    bitterexprice()
+    preludeprice()
+    mintpalprice()
+
+pricetimer()
